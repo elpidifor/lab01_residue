65e5fdde3c3efbd7cb5c2c3977ec087d6fc11034
elpidifor <elpidifor011@yandex.ru>
2018-10-08 00:49:41 +0300

    Create residue.h

---
+++ residue.h
@@ -0,0 +1,54 @@
+#pragma once
+
+using ui64 = unsigned long long int;
+
+class Residue
+{
+public:
+    Residue(ui64 a, ui64 p);
+    Residue(const Residue&);
+    Residue& operator=(const Residue&);
+    ~Residue();
+
+    bool operator == (Residue b) const;
+    bool operator < (Residue b) const;
+
+    Residue& operator += (Residue b);
+    Residue& operator -= (Residue b);
+    Residue& operator *= (Residue b);
+    Residue& operator *= (ui64 b);
+    Residue& operator /= (Residue b);
+
+    Residue operator ++(int b);
+    Residue& operator ++();
+    Residue operator --(int b);
+    Residue& operator --();
+
+    // обратный по умножению
+    // если P состовное - бросить исключение
+    Residue operator ~ () const;
+
+    // обратный по сложению
+    Residue operator - () const;
+
+    // возведение в степень b
+    Residue operator ^= (ui64 b) const;
+    
+    ui64 Mod() const;
+private:
+};
+
+Residue operator + (Residue a, Residue b);
+Residue operator - (Residue a, Residue b);
+Residue operator * (Residue a, ui64 b);
+Residue operator * (ui64 a, Residue b);
+Residue operator * (Residue a, Residue b);
+Residue operator / (Residue a, Residue b);
+
+// возведение a в степень b
+Residue operator ^ (Residue a, ui64 b);
+
+bool operator != (Residue a, Residue b);
+bool operator > (Residue a, Residue b);
+bool operator >= (Residue a, Residue b);
+bool operator <= (Residue a, Residue b);
